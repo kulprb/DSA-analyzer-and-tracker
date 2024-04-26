f93cc70664c26edf3d07dd5dac05b8643adabcd2
PRABAL JAIN <164293895+kulprb@users.noreply.github.com>
2024-04-26 11:20:06 +0530

    Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     const originalAuthorLinkedInLink = "https://www.linkedin.com/in/vishnu-patel-535961205?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     const originalAuthorLinkedInLink = "https://www.linkedin.com/in/arnav-seth-902a2626a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     const originalAuthorLinkedInLink = "https://www.linkedin.com/in/prabal-jain-579ab8179/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BJKRaq2fwS5KKa0fkVJUFLA%3D%3D"   
-    const gitHubLink = 'https://dsa-analyzer-and-tracker.vercel.app/'
+    const gitHubLink = 'https://github.com/kulprb/DSA-analyzer-and-tracker.git'
 
     return (
         <Flex
